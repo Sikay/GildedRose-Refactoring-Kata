579421e557c35b11f959835dbe51aeac5834950e
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-03 18:20:13 +0200

    refactor names

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -6,10 +6,19 @@ namespace GildedRose;
 
 final class GildedRose
 {
-    /**
-     * @var Item[]
-     */
-    private $items;
+    private const AGED_BRIE = 'Aged Brie';
+    private const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+    private const SULFURAS_HAND_OF_RAGNAROS = 'Sulfuras, Hand of Ragnaros';
+
+    private const BACKSTAGE_PASSES_TEN_DAYS_TREATMENT = 10;
+    private const BACKSTAGE_PASSES_FIVE_DAYS_TREATMENT = 5;
+
+    private const MIN_QUALITY = 0;
+    private const MAX_QUALITY = 50;
+
+    private const SELL_IN_EXPIRES = 0;
+
+    private array $items;
 
     public function __construct(array $items)
     {
@@ -19,23 +28,23 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name != self::AGED_BRIE and $item->name != self::BACKSTAGE_PASSES) {
+                if ($item->quality > self::MIN_QUALITY) {
+                    if ($item->name != self::SULFURAS_HAND_OF_RAGNAROS) {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
-                if ($item->quality < 50) {
+                if ($item->quality < self::MAX_QUALITY) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
+                    if ($item->name == self::BACKSTAGE_PASSES) {
+                        if ($item->sell_in <= self::BACKSTAGE_PASSES_TEN_DAYS_TREATMENT) {
+                            if ($item->quality < self::MAX_QUALITY) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
+                        if ($item->sell_in <= self::BACKSTAGE_PASSES_FIVE_DAYS_TREATMENT) {
+                            if ($item->quality < self::MAX_QUALITY) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
@@ -43,15 +52,15 @@ final class GildedRose
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name != self::SULFURAS_HAND_OF_RAGNAROS) {
                 $item->sell_in = $item->sell_in - 1;
             }
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->sell_in < self::SELL_IN_EXPIRES) {
+                if ($item->name != self::AGED_BRIE) {
+                    if ($item->name != self::BACKSTAGE_PASSES) {
+                        if ($item->quality > self::MIN_QUALITY) {
+                            if ($item->name != self::SULFURAS_HAND_OF_RAGNAROS) {
                                 $item->quality = $item->quality - 1;
                             }
                         }
@@ -59,7 +68,7 @@ final class GildedRose
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
-                    if ($item->quality < 50) {
+                    if ($item->quality < self::MAX_QUALITY) {
                         $item->quality = $item->quality + 1;
                     }
                 }
