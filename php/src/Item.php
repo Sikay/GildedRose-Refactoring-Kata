efd8dbc02fd4a4bd93b2ded63d8c406fc42c0d6a
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-03 18:02:22 +0200

    added requirements test

--- php/src/Item.php
+++ php/src/Item.php
@@ -6,26 +6,31 @@ namespace GildedRose;
 
 final class Item
 {
-    /**
-     * @var string
-     */
-    public $name;
 
-    /**
-     * @var int
-     */
-    public $sell_in;
-
-    /**
-     * @var int
-     */
-    public $quality;
+    public string $name;
+    public int $sell_in;
+    public int $quality;
 
     public function __construct(string $name, int $sell_in, int $quality)
     {
         $this->name = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function sellIn(): int
+    {
+        return $this->sell_in;
+    }
+
+    public function quality(): int
+    {
+        return $this->quality;
     }
 
     public function __toString(): string
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,120 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    /** @test */
+    public function should_decrease_quality_when_pass_a_day(): void
     {
-        $items = [new Item('foo', 0, 0)];
-        $gildedRose = new GildedRose($items);
+        $item = new Item('+5 Dexterity Vest', 10, 20);
+        $gildedRose = new GildedRose([$item]);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame(19, $item->quality());
+    }
+
+    /** @test */
+    public function should_decrease_sell_in_when_pass_a_day(): void
+    {
+        $item = new Item('+5 Dexterity Vest', 10, 20);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(9, $item->sellIn());
+    }
+
+    /** @test */
+    public function should_decrease_double_quality_when_pass_a_day_with_sell_in_expires(): void
+    {
+        $item = new Item('+5 Dexterity Vest', 0, 20);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(18, $item->quality());
+    }
+
+    /** @test */
+    public function should_not_decrease_quality_under_zero(): void
+    {
+        $item = new Item('+5 Dexterity Vest', 10, 0);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $item->quality());
+    }
+
+    /** @test */
+    public function should_increase_aged_brie_quality_when_pass_a_day(): void
+    {
+        $item = new Item('Aged Brie', 6, 14);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(15, $item->quality());
+    }
+
+    /** @test */
+    public function should_increase_double_aged_brie_quality_when_pass_a_day_with_sell_in_expires(): void
+    {
+        $item = new Item('Aged Brie', -2, 14);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(16, $item->quality());
+    }
+
+    /** @test */
+    public function should_not_increase_quality_over_fifty(): void
+    {
+        $item = new Item('Aged Brie', -2, 50);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(50, $item->quality());
+    }
+
+    /** @test */
+    public function should_not_change_quality_for_legendary_item(): void
+    {
+        $item = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(80, $item->quality());
+    }
+
+    /** @test */
+    public function should_not_change_sell_in_for_legendary_item(): void
+    {
+        $item = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $item->sellIn());
+    }
+
+    /** @test */
+    public function should_increase_backstage_passes_quality_when_pass_a_day_with_sell_in_over_ten(): void
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 10);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(11, $item->quality());
+    }
+
+    /** @test */
+    public function should_increase_double_backstage_passes_quality_when_pass_a_day_with_sell_in_under_ten(): void
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 17);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(19, $item->quality());
+    }
+
+    /** @test */
+    public function should_increase_triple_backstage_passes_quality_when_pass_a_day_with_sell_in_under_five(): void
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 25);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(28, $item->quality());
+    }
+
+    /** @test */
+    public function should_decrease_backstage_passes_quality_to_zero_when_pass_a_day_with_sell_in_expires(): void
+    {
+        $item = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertSame(0, $item->quality());
     }
 }
